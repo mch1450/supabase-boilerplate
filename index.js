7f2667f63438ac8f293bcb35c87c98e842f0f65d
Megan <meganc335@gmail.com>
2023-03-20 15:43:00 -0400

    Supabase test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0";
+
+const supabaseUrl = "https://fuvqupnrmsbkporizgfi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1dnF1cG5ybXNia3Bvcml6Z2ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcxMTU1NTAsImV4cCI6MTk5MjY5MTU1MH0.PQsTI0tPbOK0mbkD_bM-oGLSARYIvPlq9FkUPeTUSsk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from("books")
+  .select("*");
+
+  for (let book of books) {
+    let bookList = document.getElementById("books");
+    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td></tr>`;
+  }
+  
+}
+
+getBooks();
